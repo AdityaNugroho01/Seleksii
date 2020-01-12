357abaa828cc9ac2e4027f943e79b8a3f7317950
AdityaNugroho01 <54809205+AdityaNugroho01@users.noreply.github.com>
2020-01-12 20:18:32 +0700

    Add files via upload

---
+++ SeleksiJava.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seleksi.java;
+import java.util.Scanner;
+import static java.lang.System.in ;
+import static java.lang.System.out ;
+/**
+ *
+ * @author user
+ */
+public class SeleksiJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur ;
+        String grade ;
+        Scanner Baca = new Scanner (in);
+        out.print ("umur =");
+        umur = Baca.nextInt();
+        
+        if (umur >17 ){
+            grade = "dewasa";
+        }
+        else if (umur <17) {
+            grade = "anak-anak";
+        }
+        else {
+            grade = "kk";
+            
+        }
+        
+        out.print ("Grade ="+ grade);
+        // TODO code application logic here
+    }
+}
